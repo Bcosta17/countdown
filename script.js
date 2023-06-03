80ce5a44cd90a63e55587aebbe29940d6bafd77a
Bruno Costa <bcosta17.bc@gmail.com>
2023-06-03 12:00:59 -0300

    adicionado o JavaScript para ativar a contagem

---
+++ script.js
@@ -0,0 +1,33 @@
+const hoursEl = document.getElementById('hours')
+const minutesEl = document.getElementById('minutes')
+const secondsEl = document.getElementById('seconds')
+const millesecondsEl = document.getElementById('milleseconds')
+
+setInterval(() => {
+  let milleseconds = parseInt(millesecondsEl.textContent)
+  let seconds = parseInt(secondsEl.textContent)
+  let minutes = parseInt(minutesEl.textContent)
+  let hours = parseInt(hoursEl.textContent)
+
+  if(milleseconds > 0){
+    milleseconds--
+  } else if (seconds > 0) {
+    milleseconds = 59
+    seconds--
+  } else if (minutes > 0) {
+    milleseconds = 59
+    seconds = 59
+    minutes --
+  } else {
+    milleseconds = 59
+    seconds = 59
+    minutes = 59
+    hours --
+  }
+
+  millesecondsEl.textContent = milleseconds.toString().padStart(2, '0');
+  secondsEl.textContent = seconds.toString().padStart(2, '0');
+  minutesEl.textContent = minutes.toString().padStart(2, '0');
+  hoursEl.textContent = hours.toString().padStart(2, '0');
+
+}, 1);
